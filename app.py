be9d4b9adb6759df62e8981d9cc73765827a5ed5
Nigel <ngebodh@users.noreply.huggingface.co>
2024-02-29 21:57:36 +0000

    Updated app.py
    Cleaned up comments

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from openai import OpenAI
 import os
 import sys
 from langchain.callbacks import StreamlitCallbackHandler
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()
+# from dotenv import load_dotenv, dotenv_values
+# load_dotenv()
 
 
 if 'key' not in st.session_state:
@@ -49,7 +49,7 @@ st.title(f'ChatBot Using {selected_model}')
 
 # Set a default model
 if selected_model not in st.session_state:
-    st.session_state[selected_model] = model_links[selected_model] #"google/gemma-7b-it"
+    st.session_state[selected_model] = model_links[selected_model] 
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -76,24 +76,9 @@ if prompt := st.chat_input("What is up?"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st_callback = StreamlitCallbackHandler(st.container())
-        # st_callback =stream_handler
-
-
-        # stream = client.completions.create(
-        #     model="google/gemma-7b-it",
-        #     prompt="You are a helpful agent in a question answer exhange. Give you best answer to the questions. {prompt}",
-        #     # messages=[
-        #     #     {"role": m["role"], "content": m["content"]}
-        #     #     for m in st.session_state.messages
-        #     # ],
-        #     temperature=0.5,
-        #     stream=True,
-        #     max_tokens=3000
-        # )
-
 
         stream = client.chat.completions.create(
-            model=model_links[selected_model],#"google/gemma-7b-it",
+            model=model_links[selected_model],
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
